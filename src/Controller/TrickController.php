f333f4e6dc3a4955014e0d9128e37c62cac9cdf6
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2022-12-27 17:35:59 +0100

    feat: basic routing

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,33 +2,13 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TrickController extends AbstractController {
-
-    #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response {
-        echo 'homepage';
-
-        $tracks = [
-            ['song' => 'trick 1', 'artist' => 'alex'],
-            ['song' => 'trick 2', 'artist' => 'shashou'],
-        ];
-        return $this->render('trick/homepage.html.twig', [
-            'title' => 'Snowtricks',
-            'tracks' => $tracks,
-        ]);
-    }
-
-    #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
-    {
-        echo $slug;
-        return $this->render('trick/browse.html.twig', [
-            'genre' => $genre
-        ]);
+class TrickController
+{
+    #[Route('/')]
+    public function homepage(){
+        return new Response('Liste des tricks');
     }
 }
