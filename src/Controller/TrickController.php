14d6f3ea247e7d24ffa77960cc00bc6d3248ba88
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2022-12-27 18:13:38 +0100

    feat: homepage render

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,11 +4,27 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TrickController
+class TrickController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(){
-        return new Response('Liste des tricks');
+    public function homepage(): Response
+    {
+
+        $tricks = [
+            ['title' => 'Super flip', 'content' => 'This is a cool flip'],
+        ];
+
+        return $this->render('trick/homepage.html.twig', [
+            'title' => 'Snowtricks',
+            'tricks' => $tricks,
+        ]);
+    }
+    
+    #[Route('/browse/{slug}')]
+    public function browse($slug): Response
+    {
+        return new Response('Figure: '.$slug);
     }
 }
