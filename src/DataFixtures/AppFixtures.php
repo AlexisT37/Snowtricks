3ddbe3197f451d3cc0b4595800e43869c56e222e
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-01-13 16:54:28 +0100

    fixture to make a trick

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,22 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Trick;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $trick = new Trick();
+        $trick->setName('Ollie');
+        $trick->setDescription('The easiest trick');
+        $trick->setTrickgroup('Beginner');
+        $trick->setImageLink('https://snowboardingprofiles.com/wp-content/uploads/2015/07/how-to-do-an-ollie-on-a-snowboard.jpg');
+        $trick->setVideoLink('https://www.youtube.com/watch?v=kOyCsY4rBH0');
+        
+        $manager->persist($trick);
         $manager->flush();
     }
 }
