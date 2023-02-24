27b77864cc208ad3e1850075a41df06c76a89e19
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-02-24 16:15:09 +0100

    Update RegistrationControllerTest.php

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -2,30 +2,37 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use App\Entity\Trick;
 use App\Security\EmailVerifier;
 use PHPUnit\Framework\TestCase;
+use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\RegistrationController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationControllerTest extends TestCase
 {
-    // Fonction to test the registration of a user in App\Controller\RegistrationController;
-    public function testregister()
+
+    public function testItWorks(): void
     {
-        $this->assertEquals(1, 1);
-        // $emailVerifier = $this->createMock(EmailVerifier::class);
-        // $registrationController = new RegistrationController($emailVerifier);
-        // $request = $this->createMock(Request::class);
-        // // Create UserPasswordHasherInterface mock
-        // $userPasswordHasher = $this->createMock(UserPasswordHasherInterface::class);
-        // // create EntityManagerInterface mock
-        // $entityManager = $this->createMock(EntityManagerInterface::class);
-        // $response = $registrationController->register($request, $userPasswordHasher, $entityManager);
-        // $this->assertInstanceOf(Response::class, $response);
-        
-        // asserts that 1 == 1
+        self::assertEquals(42, 42);
     }
+
+    // function to test that when a user is instanciated, there is no error
+    public function testUserIsInstanciated(): void
+    {
+        $user = new User();
+        self::assertInstanceOf(User::class, $user);
+    }
+
+    public function testTrickIsInstanciated(): void
+    {
+        $user = new Trick();
+        self::assertInstanceOf(Trick::class, $user);
+    }
+
 }
