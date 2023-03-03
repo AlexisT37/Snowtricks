27913543c6c5b02e82e252c7b4be66902e4010be
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-03-03 13:00:34 +0100

    feat: add photo

--- migrations/Version20230303094740.php
+++ migrations/Version20230303094740.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230302132229 extends AbstractMigration
+final class Version20230303094740 extends AbstractMigration
 {
     public function getDescription(): string
     {
