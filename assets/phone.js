863008873725ab86162fedce78a95cc038c6e2e2
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-04-07 14:59:26 +0200

    feat: remove console logs and commented code

--- assets/phone.js
+++ assets/phone.js
@@ -10,16 +10,12 @@ function getScreenWidth() {
   // Function to handle screen width changes
   function handleScreenWidthChange() {
     var screenWidth = getScreenWidth();
-    console.log("Screen width: " + screenWidth);
     if (screenWidth < 768) {
-      console.log("Screen width is less than 768px");
       for (var i = 0; i < hiddenOnMobile.length; i++) {
         hiddenOnMobile[i].setAttribute('disabled', 'true');
         hiddenOnMobile[i].style.display = 'none';
       }
     } else {
-        console.log("Screen width is greater than 768px");
-    // enable the element with a class of 'hidden-on-mobile'
     for (var i = 0; i < hiddenOnMobile.length; i++) {
         hiddenOnMobile[i].setAttribute('disabled', 'false');
         hiddenOnMobile[i].style.display = 'flex';
@@ -31,15 +27,8 @@ function getScreenWidth() {
   
   // Get the initial screen width
   var screenWidth = getScreenWidth();
-  console.log("Initial screen width: " + screenWidth);
   handleScreenWidthChange();
-  console.log('initial handleScreenWidthChange() called');
   
   // Add an event listener for the window resize event
   window.addEventListener('resize', handleScreenWidthChange);
   
-
-
-// var for the width of the screen in px
-// var for selecting the element with a class of 'hidden-on-mobile'
-console.log(hiddenOnMobile);
