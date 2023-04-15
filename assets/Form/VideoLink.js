9f256a97b74960f258a6fd845f1f0955bc630060
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-04-15 15:18:50 +0200

    feat: adapt video link edit

--- assets/Form/VideoLink.js
+++ assets/Form/VideoLink.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Your code here
 
 
+console.log('VideoLink.js loaded');
+
+console.log('test before create');
+
+const createEdit = document.querySelector('#createEdit');
+
+
+var edit = false;
+    if (createEdit) {
+        // if inner html of createEdit = 'Edit your trick' then print edit
+        if (createEdit.innerHTML === 'Edit trick' || createEdit.innerHTML === 'Modifier la figure') {
+            console.log('edit');
+            edit = true;
+        } else {
+            console.log('create');
+
+        }
+        console.log('test after create');
+    }
 // create a new link
 const addVideoLinkLink = document.createElement('a')
 // add a css class to the link
@@ -16,52 +36,105 @@ addVideoLinkLink.dataset.collectionHolderClass='videolinks'
 const newLinkLi = document.createElement('li').append(addVideoLinkLink)
 
 // get the ul element
-const collectionHolder = document.querySelector('ul.videolinks');
+const collectionHolder = document.querySelector('ul.videolinks')
+
 if(collectionHolder) {
-    collectionHolder.appendChild(addVideoLinkLink);
+// append the new list item to the ul element
+collectionHolder.appendChild(addVideoLinkLink);
 }
 
-// append the new list item to the ul element
-        // add a click event listener to the link, the function will be called when the link is clicked, it takes an event as a parameter
-        const addVideoFormToCollection = (e) => {
-            // finds the closest parent element with the class name specified in the data attribute, namely 'videolinks'
-            const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
+// add a click event listener to the link, the function will be called when the link is clicked, it takes an event as a parameter
+const addVideoFormToCollectionEdit = (e) => {
+    e.preventDefault();
+    const collectionHolderClass = e.currentTarget.dataset.collectionHolderClass;
+    const collectionHolder = document.querySelector(`.${collectionHolderClass}`);
+    const parentElement = collectionHolder.parentElement;
 
-            if (collectionHolder) {
-                // create a new list item
-                const item = document.createElement('li');
-
-                // sets the innerHTML of the list item to the prototype of the collection
-                item.innerHTML = collectionHolder
-                    // access the prototype attribute of the collection
-                    .dataset
-                    .prototype
-                    // replace the __name__ placeholder with the current index of the collection
-                    .replace(
-                        /__name__/g,
-                        collectionHolder.dataset.index
-                    );
-
-                // append the new list item to the ul element
-                collectionHolder.appendChild(item);
-
-                // increment the index of the collection so that the next item will have a different index
-                collectionHolder.dataset.index++;
-
-                // create a button below the link with an id of coucou
-                const removelinkbutton = document.createElement('button');
-                removelinkbutton.classList.add('btn', 'btn-danger', 'mt-2');
-                removelinkbutton.innerHTML = "Retirer le lien";
-                // add the button as a sibling of the link
-                e.currentTarget.parentNode.appendChild(removelinkbutton);
-
-                // add a click event listener to the button
-                removelinkbutton.addEventListener("click", removeVideoFormFromCollection)
-                // if the button is clicked, the removeVideoFormFromCollection function will be called
-
-                console.log('end test video link');
+    // Count existing elements with a specific pattern in their id or for attributes
+    const existingElements = parentElement.querySelectorAll(`input[id^='trick_videolinks_']`);
+    let maxIndex = -1;
+    existingElements.forEach((inputElement) => {
+        const idMatch = inputElement.id.match(/(?<=trick_videolinks_)\d+/);
+        if (idMatch) {
+            const index = parseInt(idMatch[0], 10);
+            if (index > maxIndex) {
+                maxIndex = index;
             }
         }
+    });
+    const startIndex = maxIndex + 1;
+    console.log('startIndex', startIndex)
+
+    const item = document.createElement('li');
+    item.innerHTML = collectionHolder
+        .dataset
+        .prototype
+        .replace(
+            /__name__/g,
+            startIndex
+        )
+        // Fix the 'for' attribute of the label
+        .replace(
+            /for="trick_videolinks___name___content"/g,
+            `for="trick_videolinks_${startIndex}_content"`
+        );
+
+    collectionHolder.appendChild(item);
+    collectionHolder.dataset.index = startIndex + 1;
+
+    const removelinkbutton = document.createElement('button');
+    removelinkbutton.innerHTML = "Retirer le lien";
+    e.currentTarget.parentNode.appendChild(removelinkbutton);
+
+    removelinkbutton.addEventListener("click", removeVideoFormFromCollection)
+}
+
+const addVideoFormToCollectionCreate = (e) => {
+    e.preventDefault();
+    // finds the closest parent element with the class name specified in the data attribute, namely 'videolinks'
+	const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
+
+    // create a new list item
+    const item = document.createElement('li');
+
+
+    
+    // sets the innerHTML of the list item to the prototype of the collection
+    item.innerHTML = collectionHolder
+        // access the prototype attribute of the collection
+        .dataset
+        .prototype
+        // replace the __name__ placeholder with the current index of the collection
+        .replace(
+            /__name__/g,
+            collectionHolder.dataset.index
+        );
+
+    // append the new list item to the ul element
+    collectionHolder.appendChild(item);
+
+    // increment the index of the collection so that the next item will have a different index
+    collectionHolder.dataset.index++;
+
+
+
+    // console.log('add link');
+
+    // create a button below the link with an id of coucou
+    const removelinkbutton = document.createElement('button');
+    // add the class btn btn-danger to the button
+    removelinkbutton.classList.add('btn', 'btn-danger', 'mt-2');
+    removelinkbutton.innerHTML = "Retirer le lien";
+    // add the button as a sibling of the link
+    e.currentTarget.parentNode.appendChild(removelinkbutton);
+
+    // add a click event listener to the button
+    removelinkbutton.addEventListener("click", removeVideoFormFromCollection)
+    // if the button is clicked, the removeVideoFormFromCollection function will be called
+
+    // console.log('end test link');
+}
+
 
 
 function removeVideoFormFromCollection(e) {
@@ -75,15 +148,13 @@ function removeVideoFormFromCollection(e) {
     e.currentTarget.remove();
 }
 
-    try {
-        if (addVideoFormToCollection) {
-            // add the click event listener to the link
-            addVideoLinkLink.addEventListener("click", addVideoFormToCollection);
-        } else {
-            console.log('no add video link');
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+if (edit === true) {
+    addVideoLinkLink.addEventListener("click", addVideoFormToCollectionEdit)
+} else {
+    addVideoLinkLink.addEventListener("click", addVideoFormToCollectionCreate)
+}
+// add the click event listener to the link
+
+// console.log(addVideoLinkLink);
 
 });
